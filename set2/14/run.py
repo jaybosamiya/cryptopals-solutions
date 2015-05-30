54ca77395b1f4a028ad9de221fb2cfb302d9fdbd
Jay Bosamiya <jaybosamiya@gmail.com>
2015-05-30 15:40:15 +0530

    Add solution to 14 (slow but works)

---
+++ set2/14/run.py
@@ -0,0 +1,110 @@
+#! /usr/bin/env python
+
+from binascii import a2b_base64
+from random import randint
+from AES_128 import AES_128_ECB_decrypt, AES_128_ECB_encrypt, pkcs_7_unpad
+
+def gen_rand_data(length = 16):
+  return ''.join(chr(randint(0,255)) for i in range(length))
+
+def encryption_oracle(data):
+  global temp_remove_asap_key
+  if not hasattr(encryption_oracle, "key"):
+    encryption_oracle.key = gen_rand_data()
+    temp_remove_asap_key = encryption_oracle.key # TEMP ONLY # REMOVE ASAP
+  data = gen_rand_data(randint(0,255)) + data + a2b_base64(
+    'Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg'+
+    'aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq'+
+    'dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg'+
+    'YnkK')
+  return AES_128_ECB_encrypt(data, encryption_oracle.key, True)
+
+######################################################################################
+# Real crack begins after this
+######################################################################################
+
+def get_len_of_block_cipher(oracle): # Use probabilities and GCD to predict length of block
+  from fractions import gcd
+  length = None
+  for i in range(256):
+    l = len(oracle('A'*randint(0,256)))
+    if length == None:
+      length = l
+    length = gcd(l, length)
+  return length
+
+def is_ECB_encoded(data, block_size):
+  block_count = len(data)/block_size
+  for i in range(block_count):
+    for j in range(i+1,block_count):
+      if data[i*block_size:(i+1)*block_size] == data[j*block_size:(j+1)*block_size]:
+        return True
+  return False
+
+def verify_ECB_mode(oracle, block_size):
+  inp = 'A'*(3*block_size)
+  return is_ECB_encoded(oracle(inp),block_size)
+
+def remove_all_random_chars(oracle, block_size):
+  suffix = [None]*block_size
+  minlen = [None]*block_size
+  for i in range(block_size):
+    replies = []
+    while True:
+      r = oracle('A'*i)
+      if r in replies:
+        break
+      else:
+        replies.append(r)
+    suffix[i] = r[-block_size:]
+    minlen[i] = len(r)
+
+  def suffix_oriented_oracle(data):
+    a = len(data) / block_size
+    b = len(data) % block_size
+
+    while True:
+      r = oracle(data)
+      if len(r) == a*block_size + minlen[b]:
+        if r[-block_size:] == suffix[b]:
+          return r
+
+  return suffix_oriented_oracle
+
+def pull_next_letter(oracle, block_size, known_plaintext_prefix):
+  required_prefix_len = (block_size - (1 + len(known_plaintext_prefix))) % block_size
+  fixed_prefix = 'A'*required_prefix_len
+
+  tested_block_len = len(fixed_prefix) + len(known_plaintext_prefix) + 1
+
+  real_text = oracle(fixed_prefix)[:tested_block_len]
+
+  for i in range(256):
+    attack_text = fixed_prefix + known_plaintext_prefix + chr(i)
+    if real_text == oracle(attack_text)[:tested_block_len]:
+      return chr(i)
+
+def crack(oracle):
+  block_size = get_len_of_block_cipher(oracle)
+  print "[+] Identified block size = " + str(block_size)
+  if not verify_ECB_mode(oracle, block_size):
+    print "[-] Not ECB encryption oracle"
+    return
+  else:
+    print "[+] Verified ECB encryption"
+
+  oracle = remove_all_random_chars(oracle, block_size)
+
+  complete_message_len = len(oracle(''))
+
+  message = ''
+  for i in range(complete_message_len):
+    try:
+      message += pull_next_letter(oracle, block_size, message)
+      print message
+    except:
+      break
+
+  return pkcs_7_unpad(message)
+
+print "----- DECRYPTED TEXT START -----\n" + crack(encryption_oracle) + "------ DECRYPTED TEXT END ------"
